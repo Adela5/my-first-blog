afa32987d9081002e6fc39b387380e21debf3a55
Adéla Ryšavá <xmalys@email.cz>
2018-01-20 17:56:52 +0100

    ctvrta revize blog/templates/blog/base.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url('^$', views.post_list, name='post_list'), #^$ nic - tzn. homepage - když tam někdo přijde, ukáže mu to to, co vrátí fce post list
+    url(r'^post/(?P<pk>\d+)$',
+        views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
